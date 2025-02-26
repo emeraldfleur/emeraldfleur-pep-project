ed9e9c709c00f88fb8e191ed16b87cb640579741
Emerald Fleur Pham <emerald.pham@hey.com>
2025-02-26 18:36:40 +0000

    Added sample DAO method to DAO as template.

--- src/main/java/DAO/SocialDAO.java
+++ src/main/java/DAO/SocialDAO.java
@@ -1,9 +1,25 @@
 package DAO;
 import Model.Account;
+import Util.ConnectionUtil;
+import java.sql.*; 
 
 public class SocialDAO {
     public static boolean accountExists(Account account)
     {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            //Write SQL logic here
+            String sql = "SELECT * FROM Author;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Author author = new Author(rs.getInt("id"), rs.getString("name"));
+                authors.add(author);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return authors;
         return true;
     }
     public static Account createAccount(Account account)
@@ -17,7 +33,7 @@ public class SocialDAO {
         return true;
 
     }
-    public static Account retrieveAccountbyUserID(int userID)
+    public static Account retrieveAccountbyUserID(String username)
     {
         Account bob = new Account();
         return bob;
@@ -26,3 +42,22 @@ public class SocialDAO {
 
 
 
+/*
+ * 
+ * Connection connection = ConnectionUtil.getConnection();
+        List<Author> authors = new ArrayList<>();
+        try {
+            //Write SQL logic here
+            String sql = "SELECT * FROM Author;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Author author = new Author(rs.getInt("id"), rs.getString("name"));
+                authors.add(author);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return authors;
+    }
+ */
