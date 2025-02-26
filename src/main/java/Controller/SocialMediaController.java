d7c94c00b6604cc4921aee59d87302503bd100a3
Emerald Fleur Pham <emerald.pham@hey.com>
2025-02-26 04:10:49 +0000

    Completed additional work on Social Media Controller

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -4,33 +4,37 @@ import io.javalin.Javalin;
 import io.javalin.http.Context;
 import Model.Account;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
- * found in readme.md as well as the test cases. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
 public class SocialMediaController {
-    /**
-     * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
-     * suite must receive a Javalin object from this method.
-     * @return a Javalin app object which defines the behavior of the Javalin controller.
-     */
+
     public Javalin startAPI() {
         Javalin app = Javalin.create();
 
         app.post("/register", this::registrationHandler);
         app.post("/login", this::loginHandler);
+        app.post("/message", this::postMessageHandler);
+
+        app.get("/messages", this::getMessageHandler);
+
         return app;
     }
 
 
-    private void registrationHandler(Context context) {
+    private void registrationHandler(Context context) 
+    {
         
     }
 
-    private void loginHandler(Context context) {
+    private void loginHandler(Context context) 
+    {
         
     }
-
+    private void postMessageHandler(Context context)
+    {
+        
+    }
+    private void getMessageHandler(Context context)
+    {
+        
+    }
 
 }
